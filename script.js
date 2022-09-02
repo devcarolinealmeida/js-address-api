b9520138fd22b26afca83b69ea3d46875c6504c5
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-02 12:45:03 +0200

    add async function and await

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var searchAddress = fetch("https://viacep.com.br/ws/01001011/json/")
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('This address dont exist')
-        } else
-            console.log(r)
-    })
-    .catch(erro => console.log(erro));
+async function searchAddress() {
+    var searchCap = await fetch("https://viacep.com.br/ws/01001000/json/")
+    var searchCapConverted = await searchCap.json()
+    console.log(searchCapConverted)
+}
+searchAddress()
 
-console.log(searchAddress)
+
+
+
+// var searchAddress = fetch("https://viacep.com.br/ws/01001000/json/")
+//     .then(response => response.json())
+//     .then(r => {
+//         if (r.erro) {
+//             throw Error('This address dont exist')
+//         } else
+//             console.log(r)
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(message => console.log('Concluded'));
+
+// console.log(searchAddress)
 

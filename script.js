57f2f42f6e486cf374355fb71f22ec70cfd47077
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-02 10:15:53 +0200

    testing throw Error('This address dont exist')

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var searchAddress = fetch('https://viacep.com.br/ws/01001000/json')
-.then(answer => answer.json())
-.then(a => console.log(a));
+var searchAddress = fetch("https://viacep.com.br/ws/01001011/json/")
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('This address dont exist')
+        } else
+            console.log(r)
+    })
+    .catch(erro => console.log(erro));
 
 console.log(searchAddress)
+

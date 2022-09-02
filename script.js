6cc582c3f739752dcacc6efdc88b0072d025143b
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-02 13:01:57 +0200

    add try, catch and Error message

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+// async await
+
 async function searchAddress() {
-    var searchCap = await fetch("https://viacep.com.br/ws/01001000/json/")
-    var searchCapConverted = await searchCap.json()
-    console.log(searchCapConverted)
+  try {
+    var searchCap = await fetch("https://viacep.com.br/ws/01001250/json/");
+    var searchCapConverted = await searchCap.json();
+    if (searchCapConverted.erro) {
+        throw Error('Address not found')
+    }
+    console.log(searchCapConverted);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
-searchAddress()
+searchAddress();
 
 
 
+//-------------------------------------------------
+// using .then and promisses
+// This code makes a request to an API through its link together with the fetch method and then converts the result to JSON.
 
-// var searchAddress = fetch("https://viacep.com.br/ws/01001000/json/")
+//var searchAddress = fetch("https://viacep.com.br/ws/01001000/json/")
 //     .then(response => response.json())
 //     .then(r => {
 //         if (r.erro) {
@@ -20,4 +32,3 @@ searchAddress()
 //     .finally(message => console.log('Concluded'));
 
 // console.log(searchAddress)
-
